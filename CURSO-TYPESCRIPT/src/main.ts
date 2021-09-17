0d16c9b8bdefba3cd4f63fd63de257016f0cf920
Eder Sena <eder.sena@live.com>
2021-09-16 22:25:32 -0300

    interface

--- CURSO-TYPESCRIPT/src/main.ts
+++ CURSO-TYPESCRIPT/src/main.ts
@@ -1,1 +1,18 @@
-// BASE ARQUIVO CURSO CATALINE
+// show the user's city
+type UF = 'PE' | 'SP' | 'RJ'
+
+interface User { 
+    name: string, 
+    address: { 
+      city: string,
+      UF: UF
+    }
+    sayHello: () => void
+} // interface
+
+function showCity(user: User) {
+  user.address.UF = 'PE'
+  user.sayHello = () => console.log('Hello')
+  return user.address.city
+}
+showCity({ name: 'John', address: { city: 'London', UF: 'RJ' }, sayHello: () => console.log('Hello') }) 
