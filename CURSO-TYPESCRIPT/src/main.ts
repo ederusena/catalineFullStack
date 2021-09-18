ca7d150bd8a1c02bf434641770bee35c8522926a
Eder Sena <eder.sena@live.com>
2021-09-18 12:10:40 -0300

    propriedade readonly

--- CURSO-TYPESCRIPT/src/main.ts
+++ CURSO-TYPESCRIPT/src/main.ts
@@ -1,1 +1,13 @@
 // BASE ARQUIVO CURSO CATALINE
+interface User {
+  readonly name: string;
+  age: number;
+}
+
+let user: User = {
+  name: "John",
+  age: 30
+};
+
+user.age = 40;
+user.name = "Pete";
