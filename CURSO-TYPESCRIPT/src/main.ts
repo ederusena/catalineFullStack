99153c252c76a272d72e2b90c0af57c58c064b37
Eder Sena <eder.sena@live.com>
2021-09-18 12:58:22 -0300

    criacao de paths no typescript e solucao de problemas
    // "baseUrl": "./src",                             /* Base directory to resolve non-absolute module names. */
    // "paths": {
    //   "@utils/*":["utils/*"],
    //   "@http/*":["controllers/http/*"],
    //   "@/*": ["./*"]
    // },
    
    // npm i tsconfig-paths -D

--- CURSO-TYPESCRIPT/src/main.ts
+++ CURSO-TYPESCRIPT/src/main.ts
@@ -1,1 +1,12 @@
-// BASE ARQUIVO CURSO CATALINE
+// "baseUrl": "./src",                             /* Base directory to resolve non-absolute module names. */
+// "paths": {
+//   "@utils/*":["utils/*"],
+//   "@http/*":["controllers/http/*"],
+//   "@/*": ["./*"]
+// },  
+
+// npm i tsconfig-paths -D
+
+import users from '@http/Users'
+
+users()
