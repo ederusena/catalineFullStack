10b0c18e80357f2aa95f32b03d8b2c2a175511d6
Eder Sena <eder.sena@live.com>
2021-11-10 10:10:50 -0300

    Data

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,23 @@
 <template>
-  <h1>YOOO</h1>
+  <h1>{{ person.name }}</h1>
 </template>
 
 <script lang='ts'>
 import { defineComponent } from 'vue';
 
+interface Person {
+  name: string;
+  age: number;
+}
+
 export default defineComponent({
-  
+  data() {
+    return {
+      person: {
+        name: '',
+        age: 0,
+      } as Person,
+    };
+  },
 })
 </script>
