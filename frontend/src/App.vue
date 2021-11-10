389a4b25d9a3ae0eaef42eedff052353ea7a49f9
Eder Sena <eder.sena@live.com>
2021-11-10 10:48:42 -0300

    METHODS
    CHAMAR FUNCAO AO EFETUAR UMA AÇÃO. EX UM CLICK DE BOTAO

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,32 @@
 <template>
-  <h1>YOOO</h1>
+  <div>
+    <h1>{{ count }}</h1>
+
+    < button @click="increment">Incrementar</button>
+    <button @click="decrement">Decrementar</button>
+    <button @click="reset">Resetar</button>
+  </div>
 </template>
 
 <script lang='ts'>
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    decrement() {
+      this.count--;
+    },
+    reset() {
+      this.count = 0;
+    },
+  },
 })
 </script>
