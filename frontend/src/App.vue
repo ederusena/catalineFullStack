cf7a2eff1351353819ab8d6608e5ef411381e57f
Eder Sena <eder.sena@live.com>
2021-11-10 15:44:07 -0300

    BeroreCreated e Created

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,26 @@
 <template>
-  <h1>YOOO</h1>
+  <h1>{{message}}</h1>
 </template>
 
 <script lang='ts'>
 import { defineComponent } from 'vue';
 
 export default defineComponent({
+  data() {
+    return {
+      message: 'Hello Vue!'
+    };
+  },
+  beforeCreate() {
+    // beforeCreate is called before the component is mounted.
   
+    // nao tem acesso ao Data aqui porque nao foi montado ainda
+    console.log('beforeCreate');
+  },
+  created() {
+    // The component has been mounted.
+     // tem acesso ao Data aqui porque foi montado
+    console.log('created');
+  },
 })
 </script>
