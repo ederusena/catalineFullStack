260ae363d5d2ee3ca91a3ef676dd5c3f2fe5a217
Eder Sena <eder.sena@live.com>
2021-11-10 15:03:39 -0300

    exercicio
    Compontes watch

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,52 @@
 <template>
-  <h1>YOOO</h1>
+  <div>
+    <p v-for="todo in doneTodos" :key="todo.text">
+      {{ todo.text }}
+    </p>
+    <button @click="checkAllTodos">Finalizar</button>
+  </div>
 </template>
 
-<script lang='ts'>
-import { defineComponent } from 'vue';
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+interface Todo {
+  text: string
+  done: boolean
+}
 
 export default defineComponent({
-  
+  data() {
+    return {
+      todos: [] as Todo[]
+    }
+  },
+  computed: {
+    doneTodos(): Todo[] {
+      return this.todos.filter((todo) => todo.done)
+    }
+  },
+  watch: {
+    todos(newValue: Todo[]) {
+      const isFinished = newValue.every((todo) => todo.done)
+
+      if (isFinished){
+        alert('Todos os todos foram finalizados!');
+      }
+  },
+  created() {
+    this.todos = [
+      { text: 'Estudar Typescript', done: true },
+      { text: 'Lavar os pratos', done: false },
+      { text: 'Aprender Nuxt.js', done: true }
+    ],
+  },
+  methods: {
+    checkAllTodos() {
+      this.todos = this.todos.map(({ text }) => {
+        return { text, done: true }
+      })
+    },
+  },
 })
 </script>
