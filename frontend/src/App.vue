e45f07546b64c1a641bc288f63d0f39432533c0c
Eder Sena <eder.sena@live.com>
2021-11-11 16:06:37 -0300

    v-bind

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,43 @@
 <template>
-  <h1>YOOO</h1>
+  <div>
+    <a
+      v-bind:href="product.url"
+      v-bind:class="handleClass"
+    >
+      {{ product.name }}
+    </a>
+
+    <button v-on:click="!product.stock">Buy product</button>
+  </div>
 </template>
 
-<script lang='ts'>
-import { defineComponent } from 'vue';
+<script lang="ts">
+import { defineComponent } from 'vue'
 
 export default defineComponent({
-  
+  data() {
+    return {
+      product: {
+        name: 'Camisa',
+        url: 'http://www.google.com',
+        stock: true
+      }
+    }
+  },
+  computed: {
+    handleClass(): string{
+      return this.product.stock ? 'success' : 'danger'
+    },
+  }
 })
 </script>
+
+<style>
+.success {
+  color: green;
+}
+
+.danger {
+  color: red;
+}
+</style>
