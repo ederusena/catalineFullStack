8ddff3c83aab7925be6cf2dd81c162ab6a47b966
Eder Sena <eder.sena@live.com>
2021-11-11 12:29:29 -0300

    beforeUnmount e unmounted

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,30 @@
 <template>
-  <h1>{{message}}</h1>
+  <div>
+    <h1>{{ count }}</h1>
+    <button @click="count++">Incrementar</button>
+    <button @click="DestroyComponent">Destroy</button>
+  </div>
 </template>
 
 <script lang='ts'>
-import { defineComponent } from 'vue';
+import { defineComponent } from 'vue'
 
 export default defineComponent({
   data() {
     return {
-      message: 'Hello Vue!'
-    };
+      count: 0
+    }
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount')
+  },
+  unmounted() {
+    console.log('unmounted')
+  },
+  methods: {
+    DestroyComponent() {
+      this.$.appContext.app.unmount(this.$el)
+    }
   }
 })
 </script>
