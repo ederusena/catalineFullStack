b8ddfe4dcea9299be77b8c30861cd18b38cd3a47
Eder Sena <eder.sena@live.com>
2021-11-11 16:37:03 -0300

    v-on
    once
    prevent
    keyup

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,21 @@
 <template>
-  <h1>YOOO</h1>
+  <div>
+    <a href="#interno" v-on:click.once.prevent="something"> Link Interno </a>
+    <br />
+    <input type="text" @keyup.enter="something" />
+    <br />
+    <input type="text" @keyup.k="something" />
+  </div>
 </template>
 
-<script lang='ts'>
-import { defineComponent } from 'vue';
+<script lang="ts">
+import { defineComponent } from 'vue'
 
 export default defineComponent({
-  
+  methods: {
+    something() {
+      console.log('Clicked!')
+    }
+  }
 })
 </script>
