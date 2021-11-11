a85e41d82992914923a0cab3b3060121a3d8cd41
Eder Sena <eder.sena@live.com>
2021-11-11 17:11:10 -0300

    v-if
    v-else
    v-else-if

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    
+    <h1 v-if="gender === 'Masculino'">Masculino</h1>
+    <h1 v-else-if="gender === 'Feminino'">Feminino</h1>
+    <h1 v-else>Outros</h1>
   </div>
 </template>
 
@@ -8,6 +10,10 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  
+  data() {
+    return {
+      gender: 'Masculino'
+    };
+  },
 })
 </script>
