bc06d44537356d1125cfa52bcd419f458bdc98aa
Eder Sena <eder.sena@live.com>
2021-11-11 12:02:39 -0300

    BeforeMount e mounted

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,33 @@
 <template>
-  <h1>{{message}}</h1>
+  <h1>{{ message }}</h1>
 </template>
 
 <script lang='ts'>
-import { defineComponent } from 'vue';
+import { defineComponent } from 'vue'
 
 export default defineComponent({
   data() {
     return {
       message: 'Hello Vue!'
-    };
+    }
+  },
+  beforeCreate() {
+    // Nao tem acesso ao DOM nem ao DATA
+    console.log(`beforeCreate, Data:${this.message}, DOM: ${this.$el}`)
+  },
+  created() {
+    // Nao tem acesso ao DOM e tem acesso ao DATA
+    console.log(`created, Data:${this.message}, DOM: ${this.$el}`)
+  },
+  beforeMount() {
+    // Nao tem acesso ao DOM e tem acesso ao Data
+    console.log(`beforeMount, Data:${this.message}, DOM: ${this.$el}`)
+  },
+  mounted() {
+    // Tem acesso ao DOM e tem acesso ao Data
+    this.$el.style.color = 'red'
+    console.log(`mounted , Data:${this.message}, 
+    DOM: ${this.$el}`)
   }
 })
 </script>
