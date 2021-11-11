10bfc15ab6900b02609f7ad539c529e0fdf66cbc
Eder Sena <eder.sena@live.com>
2021-11-11 16:40:33 -0300

    v-once
    inibi que um valor seja reativo

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,20 @@
 <template>
-  <h1>YOOO</h1>
+  <div>
+    <p v-once>Estoque inicial: {{ stock }}</p>
+    <p>Estoque atual: {{ stock }}</p>
+
+    <button @click="stock++">Incrementar</button>
+  </div>
 </template>
 
 <script lang='ts'>
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  
+  data() {
+    return {
+      stock: 1
+    }
+  },
 })
 </script>
