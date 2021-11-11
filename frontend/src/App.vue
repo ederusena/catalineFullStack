290d2ef4c768387ee3789dd4328c31917c8d4c84
Eder Sena <eder.sena@live.com>
2021-11-11 17:20:20 -0300

    v-show
    diferença entre v-if e v-show
    vide notion.so

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    
+    <h1 v-show="gender === 'Masculino'">Masculino</h1>
   </div>
 </template>
 
@@ -8,6 +8,10 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  
+  data() {
+    return {
+      gender: 'Masculino'
+    }
+  }
 })
 </script>
