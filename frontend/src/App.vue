036e2f16350a0d8ecf68ea64e643f38f175c4633
Eder Sena <eder.sena@live.com>
2021-11-12 12:27:30 -0300

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div>
-    
+    <h1 class="static">
+      Cataline
+    </h1>
   </div>
 </template>
 
-<script lang='ts'>
+<script lang="ts">
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  
+  data() {
+    return {
+      isActive: true,
+      hasError: false,
+    };
+  },
 })
 </script>
