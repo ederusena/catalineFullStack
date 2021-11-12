618e0c2d43cecdd5389d81c446535c5a39ed3ef6
Eder Sena <eder.sena@live.com>
2021-11-12 00:26:42 -0300

    directiva personalizada

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
-    
+    <h1 v-highlight="'red'">Hello brows!</h1>
+    <h1 v-highlight.background.shadow="'red'">Hello brows!</h1>
+
   </div>
 </template>
 
-<script lang='ts'>
+<script lang="ts">
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  
 })
 </script>
