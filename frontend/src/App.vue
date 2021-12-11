8edaad51545680d3441216e038d0ae84563ec2ee
Eder Sena <eder.sena@live.com>
2021-12-10 23:23:16 -0300

    componentens

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,30 @@
 <template>
   <div>
-    
+    <ButtonStyled text="Facebook">
+      <template v-slot:before>
+        <i class="fab fa-facebook-f"></i>
+      </template>
+
+      <template v-slot:after>
+        <i class="fab fa-facebook"></i>
+      </template>
+    </ButtonStyled>
+
+    <ButtonStyled text="Facebook" color="success">
+      <template v-slot:after>
+        <i class="fab fa-facebook-f"></i>
+      </template>
+    </ButtonStyled>
   </div>
 </template>
 
-<script lang='ts'>
-import { defineComponent } from 'vue';
+<script lang="ts">
+import { defineComponent } from 'vue'
+import ButtonStyled from './components/ButtonStyled.vue'
 
 export default defineComponent({
-  
+  components: {
+    ButtonStyled
+  },
 })
 </script>
