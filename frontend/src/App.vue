0d708123e4436c009b26c7729643923664bbfff1
Eder Sena <eder.sena@live.com>
2021-12-11 00:04:53 -0300

    Components clears

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,9 @@
 <template>
-  <div>
-    <ButtonStyled text="Facebook">
-      <template v-slot:before>
-        <i class="fab fa-facebook-f"></i>
-      </template>
-
-      <template v-slot:after>
-        <i class="fab fa-facebook"></i>
-      </template>
-    </ButtonStyled>
-
-    <ButtonStyled text="Facebook" color="success">
-      <template v-slot:after>
-        <i class="fab fa-facebook-f"></i>
-      </template>
-    </ButtonStyled>
-  </div>
+  <div>oi</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import ButtonStyled from './components/ButtonStyled.vue'
 
-export default defineComponent({
-  components: {
-    ButtonStyled
-  },
-})
+export default defineComponent({})
 </script>
