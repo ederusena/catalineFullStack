70e730b0b319700c4d0fb9bf6ffb6a0d4dcdd9f2
Eder Sena <eder.sena@live.com>
2021-12-11 00:02:29 -0300

    components dinamicos

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,33 @@
 <template>
   <div>
-    <ButtonStyled text="Facebook">
-      <template v-slot:before>
-        <i class="fab fa-facebook-f"></i>
-      </template>
-
-      <template v-slot:after>
-        <i class="fab fa-facebook"></i>
-      </template>
-    </ButtonStyled>
-
-    <ButtonStyled text="Facebook" color="success">
-      <template v-slot:after>
-        <i class="fab fa-facebook-f"></i>
-      </template>
-    </ButtonStyled>
+    <ul>
+      <li @click="currentView = 'Home'">Home</li>
+      <li @click="currentView = 'About'">Sobre</li>
+      <li @click="currentView = 'Contact'">Contato</li>
+    </ul>
+    <component :is="currentView" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import ButtonStyled from './components/ButtonStyled.vue'
+
+import Home from './pages/Home.vue'
+import About from './pages/About.vue'
+import Contact from './pages/Contact.vue'
 
 export default defineComponent({
   components: {
-    ButtonStyled
+    Home,
+    About,
+    Contact
   },
+  data() {
+    return {
+      currentView: 'Home'
+    }
+  }
 })
 </script>
+
+<style scoped></style>
