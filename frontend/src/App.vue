b50b46ba0d012ec8d06c4e3d00e1eeb30062ed1f
Eder Sena <eder.sena@live.com>
2021-12-13 23:05:46 -0300

    CSS
    Colunas e linhas - grid-template-columns: 1fr 3fr;
      grid-template-rows: 200px 200px;
    Espaçamento - grid-gap / grid-row-gap / grid-column-gap
    Fluxo Automatico - grid-auto-flow: column;
    Repetição automatica - grid-template-columns: repeat(10, 1fr);

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,60 @@
 <template>
-  <h1>YOOO</h1>
+  <div class="main">
+    <div class="container">
+      <div class="item"></div>
+      <div class="item"></div>
+      <div class="item"></div>
+      <div class="item"></div>
+    </div>
+
+    <div class="container2">
+      <div class="item2"></div>
+      <div class="item2"></div>
+      <div class="item2"></div>
+      <div class="item2"></div>
+    </div>
+  </div>
 </template>
 
-<script lang='ts'>
-import { defineComponent } from 'vue';
+<script lang="ts">
+import { defineComponent } from 'vue'
 
-export default defineComponent({
-  
-})
+export default defineComponent({})
 </script>
+
+<style>
+.container2 {
+  grid-gap: 1rem;
+  display: grid;
+  grid-auto-flow: column;
+}
+
+.item2 {
+  background: rgba(255, 255, 255, 0.5);
+  border-radius: 5px;
+  height: 30px;
+}
+
+.main {
+  background: #3494e6;
+  height: 100vh;
+  overflow-y: hidden;
+  font-family: sans-serif;
+}
+
+.container {
+  margin: 2rem;
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  grid-template-rows: 200px 200px;
+  grid-row-gap: 1rem;
+  grid-column-gap: 1rem;
+  /* grid-gap: 1rem; */
+}
+
+.item {
+  background: rgba(255, 255, 255, 0.5);
+  border-radius: 5px;
+  border: 1px solid #000;
+}
+</style>
